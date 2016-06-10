392d0f70d53acc90d717a0652edf3157b031db1d
Alireza Omidi <alireza530@gmail.com>
2016-06-10 20:19:43 +0430

    added get_events function; modified README

--- README.md
+++ README.md
@@ -2,11 +2,24 @@
 
 This script takes **year**, **month** and **day** of an specific date and scrapes [time.ir](http://time.ir) for events.
 
-## Install & Run
+## Install
 ```bash
 $ git clone https://github.com/alirezaomidi/iran-calendar-events.git
 $ cd iran-calendar-events/
-$ python3 scraper.py 1395 09 16
+```
+
+## Use
+You can use the script in two ways:
+1. In `python3`. The `get_events` function returns a list of events:
+```python
+>>> import scraper
+>>> scraper.get_events(1395, 1, 6)
+['روز امید، روز شادباش نویسی', 'زادروز آشو زرتشت، اَبَراِنسان بزرگ تاریخ']
+```
+
+2. In `bash`:
+```bash
+$ python3 scraper.py 1395 9 16
 روز دانشجو
 ```
 
--- scraper.py
+++ scraper.py
@@ -4,26 +4,35 @@ from bs4 import BeautifulSoup
 
 BASE_URL = 'http://www.time.ir/fa/event/list/0/'
 
-if __name__ == '__main__':
+def get_events(year, month, day):
 
-    # TODO: check for args validity
+    url = BASE_URL + '%4d/%02d/%02d' % (year, month, day)
 
-    year, month, day = sys.argv[1:]
-
-    URL = BASE_URL + '%s/%s/%s' % (year, month, day)
-
-    r = requests.get(URL)
+    r = requests.get(url)
 
     if r.ok:
         bs = BeautifulSoup(r.text, 'html5lib')
         ul = [i for i in bs.ul.findChildren() if str(i).startswith('<li')]
+        events = []
         for li in ul:
-            events = li.contents[2:-1]
-            to_print = []
-            for e in events:
-                if e.string:
-                    to_print += e.string.strip().split()
-            print(' '.join(to_print))
+            tags = li.contents[2:]
+            for t in tags:
+                event = []
+                if t.string:
+                    event += t.string.strip().split()
+                if event:
+                    events.append(' '.join(event))
+        return events
     else:
         # TODO: if the request was not ok: do sth
         pass
+
+if __name__ == '__main__':
+
+    # TODO: check for args validity
+    year, month, day = map(int, sys.argv[1:])
+
+    events = get_events(year, month, day)
+
+    for e in events:
+        print(e)
